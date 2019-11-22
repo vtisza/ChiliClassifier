8bf166892488aa060ae9d0d9969cc25c45e1448d
Viktor <vtisza@gmail.com>
2019-11-22 19:35:26 +0100

    rebuild webpage

---
+++ build/precache-manifest.5c44952cdc122078a937ed536f465b44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47ffcb27101e52a45188bfd4c030cb41",
+    "url": "/react-ml-app/index.html"
+  },
+  {
+    "revision": "d4c98997737b647c9ae5",
+    "url": "/react-ml-app/static/js/2.3d6fe8bc.chunk.js"
+  },
+  {
+    "revision": "16a8413d7c188ceca752",
+    "url": "/react-ml-app/static/js/main.5d14082c.chunk.js"
+  },
+  {
+    "revision": "05647566c567bdcd306a",
+    "url": "/react-ml-app/static/js/runtime-main.b2aa0be8.js"
+  },
+  {
+    "revision": "b9c461d039dee7e1653c9c5c60a1167b",
+    "url": "/react-ml-app/static/media/chili-resnet18.b9c461d0.onnx"
+  }
+]);
