555a09e5de23d9419cca7024cc0a5a403cdcfc13
Viktor <vtisza@gmail.com>
2019-11-23 09:48:27 +0100

    Updates

---
+++ precache-manifest.0beaa7f9a1e811338792f8d2339e90e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782ebca46eec491ed302f7bee28f9849",
+    "url": "/ChiliClassifier/index.html"
+  },
+  {
+    "revision": "b955ad6637f901d7a6f0",
+    "url": "/ChiliClassifier/static/js/2.153dfeee.chunk.js"
+  },
+  {
+    "revision": "51782e80b288aff1d600",
+    "url": "/ChiliClassifier/static/js/main.6a8712e6.chunk.js"
+  },
+  {
+    "revision": "3659e99fdb1c2b8b0001",
+    "url": "/ChiliClassifier/static/js/runtime-main.f3b12f3d.js"
+  },
+  {
+    "revision": "b9c461d039dee7e1653c9c5c60a1167b",
+    "url": "/ChiliClassifier/static/media/chili-resnet18.b9c461d0.onnx"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ml-app/precache-manifest.5c44952cdc122078a937ed536f465b44.js"
+  "/ChiliClassifier/precache-manifest.0beaa7f9a1e811338792f8d2339e90e4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ml-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChiliClassifier/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
